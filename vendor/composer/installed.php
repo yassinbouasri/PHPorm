bb80d911b9defd0b8dbce0a08b2f87d5d3c6d964
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2024-11-13 14:58:19 +0100

    New ORM project: PHPorm

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2146ca8608d738ea4bfb8a3a04c3e9e67ec7e79a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2146ca8608d738ea4bfb8a3a04c3e9e67ec7e79a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
