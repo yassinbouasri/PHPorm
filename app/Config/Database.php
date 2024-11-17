b4062b5241f435e0d341872776323e6e09db57bc
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2024-11-17 17:39:35 +0100

    Working on database singleton, and layout

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -8,17 +8,17 @@ use PDO;
 
 class Database
 {
-    private const string HOST = 'localhost';
-    private const string USER = 'root';
-    private const string PASSWORD = '';
-    private const string DBNAME = "";
     private static ?self $instance = null;
     private static ?PDO $cnn = null;
+    private string $host = 'localhost';
+    private string $user = 'root';
+    private string $password = '';
+    private string $dbname = "";
 
     public function __construct()
     {
-        $dsn = "mysql:host=" . self::HOST . ";dbname=" . self::DBNAME . ";charset=utf8mb4";
-        self::$cnn = new PDO($dsn, self::USER, self::PASSWORD);
+        $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->dbname . ";charset=utf8mb4";
+        self::$cnn = new PDO($dsn, $this->user, $this->password);
         self::$cnn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     }
@@ -29,6 +29,26 @@ class Database
             self::$instance = new self();
         }
         return self::$instance;
+    }
+
+    public function setHost(string $host): void
+    {
+        $this->host = $host;
+    }
+
+    public function setUser(string $user): void
+    {
+        $this->user = $user;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function setDbname(string $dbname): void
+    {
+        $this->dbname = $dbname;
     }
 
     public function getConnection(): PDO
