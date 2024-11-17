0d5cb2232fb428f9fe071c0f0af69c3040d74abe
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2024-11-17 18:10:23 +0100

    Working on dynamic layout

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -6,25 +6,25 @@ abstract class Controller
 {
     private const string LOCATION = ROOT_DIR . "resources/views/";
 
-    public function render(string $view, array $params = [], bool $haveLayout = false )
+    public function render(string $view, array $params = [], bool $haveLayout = false): void
     {
-        $layout = "";
-        $content = file_get_contents( self::LOCATION . $view . ".weave.html");
-
         extract($params);
 
+        if (!$haveLayout) {
+            require_once self::LOCATION . $view . ".weave.html";
+        }
+
         if ($haveLayout) {
+            $content = file_get_contents(self::LOCATION . $view . ".weave.html");
             $layout = file_get_contents(self::LOCATION . "layout.html");
             $lines = explode("\n", $layout);
             foreach ($lines as $key => $value) {
 
-                $layout = str_replace("{{content}}", $content , $layout);
-
+                $layout = str_replace("{{content}}", $content, $layout);
 
             }
-
+            echo $layout;
         }
 
-        echo $layout;
     }
 }
